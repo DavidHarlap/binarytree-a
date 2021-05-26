f170057f2082470724d9f3a25877ed9f3a70778b
DavidHarlap <adavid12@gmail.com>
2021-05-26 19:14:54 +0300

    add basic test

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+#include "doctest.h"
+#include "BinaryTree.hpp"
+using namespace std;
+
+TEST_CASE("binary tree"){
+    for(int i=0;i<23;i++){
+        CHECK_EQ(1,1);
+
+    }
+}
